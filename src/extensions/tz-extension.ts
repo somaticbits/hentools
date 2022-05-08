a13e67e39b05d877a2bbe2eea91196ad16ffded2
d-pettersson <david.pettersson@code.berlin>
2022-05-08 18:14:31 +0200

    Fix async

--- src/extensions/tz-extension.ts
+++ src/extensions/tz-extension.ts
@@ -9,32 +9,32 @@ module.exports = (toolbox: GluegunToolbox) => {
 
   let secretKey: { key: string; tzAddress: string } | false = false
 
-  const getSecretKey = async (): Promise<
-    { key: string; tzAddress: string } | false
-  > => {
+  const getSecretKey = async (): Promise<{ key: string; tzAddress: string } | false> => {
     if (secretKey) return secretKey
     secretKey = await readSecretKey()
     return secretKey
   }
 
-  const readSecretKey = async (): Promise<
-    { key: string; tzAddress: string } | false
-  > => {
+  const readSecretKey = async (): Promise<{ key: string; tzAddress: string } | false> => {
     return (
       filesystem.exists(TEZOS_CONFIG) &&
-      filesystem.readAsync(TEZOS_CONFIG).then(res => JSON.parse(res))
+      await filesystem.readAsync(TEZOS_CONFIG).then(res => JSON.parse(res))
     )
   }
 
   const saveSecretKey = async (key): Promise<void> => {
-    const Tezos = new TezosToolkit(config.hentools.rpcURL)
+    try {
+      const Tezos = new TezosToolkit(config.hentools.rpcURL)
 
-    Tezos.setProvider({ signer: new InMemorySigner(key) })
+      Tezos.setProvider({ signer: new InMemorySigner(key) })
 
-    const tzAddress = await Tezos.wallet.pkh()
+      const tzAddress = await Tezos.wallet.pkh()
 
-    const wallet = { key: `${key}`, tzAddress: `${tzAddress}` }
-    return filesystem.writeAsync(TEZOS_CONFIG, wallet)
+      const wallet = { key: `${key}`, tzAddress: `${tzAddress}` }
+      return filesystem.writeAsync(TEZOS_CONFIG, wallet)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   const resetKey = async (): Promise<void> => {
