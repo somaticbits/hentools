48c7a141d879609c5ffbe4e30a9ed7339c7f6b4e
d-pettersson <david.pettersson@code.berlin>
2022-05-08 18:10:26 +0200

    Add integration tests

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -1,14 +1,107 @@
 const { system, filesystem } = require('gluegun')
-
 const src = filesystem.path(__dirname, '..')
+const stdin = require('mock-stdin')
+import { generateConfig } from './helpers'
+
+jest.setTimeout(20000)
 
 const cli = async cmd =>
-  system.run('node ' + filesystem.path(src, 'bin', 'hentools') + ` ${cmd}`)
+    system.run('node ' + filesystem.path(src, 'bin', 'hentools') + ` ${cmd}`,)
 
-// test('outputs version', async () => {
-//   const output = await cli('--version')
-//   expect(output).toContain('0.0.1')
-// })
+const TEZOS_CONFIG = `${filesystem.cwd()}/.config/tezos.config.js`
+const wallet = {
+  key: 'edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq',
+  tzAddress: 'tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb'
+}
+
+const testContracts = {
+  operatorContract: 'KT1GNDXKc9xzHAhGvtoc1ZmxsJL5RJJnxGV3',
+  marketplaceContract: 'KT1FpQVtEFGWEE37JsyqraZ9RRFceGPAtMpa',
+  minterContract: 'KT1Sa1R67Heci6kBgqUZCXrC1CgpFQEFH9BQ'
+}
+const contracts = {
+  operatorContract: 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton',
+  marketplaceContract: 'KT1PHubm9HtyQEJ4BBpMTVomq6mhbfNZ9z5w',
+  minterContract: 'KT1Hkg5qeNhfwpKW4fXvq7HGZB9z2EnmCCA9'
+}
+
+beforeAll(() => generateConfig(testContracts.operatorContract,
+                                   testContracts.marketplaceContract,
+                                   testContracts.minterContract))
+afterAll(() => {
+  generateConfig(contracts.operatorContract,
+                 contracts.marketplaceContract,
+                 contracts.minterContract)
+})
+
+describe('setup module', () => {
+  let io = null
+  beforeAll(() => (io = stdin.stdin()))
+  afterAll(() => {
+    io.restore()
+    filesystem.remove(TEZOS_CONFIG)
+  })
+
+  const key = {
+    enter: '\x0D',
+  }
+
+  test('it should ask for a key', async done => {
+    const sendInput = async () => {
+      io.send(wallet.key)
+      io.send(key.enter)
+    }
+    setTimeout(() => sendInput().then(), 1000)
+    filesystem.remove(TEZOS_CONFIG)
+    const output = await cli('setup')
+    expect(output).toContain('Secret key set.')
+  })
+
+  test('we already have set a key', async () => {
+    await filesystem.writeAsync(TEZOS_CONFIG, wallet)
+    const output = await cli('setup')
+    expect(output).toContain('Secret key already set.')
+  })
+
+  test('it should reset the key', async () => {
+    await filesystem.writeAsync(TEZOS_CONFIG, wallet)
+    const output = await cli('setup -r')
+    expect(output).toContain('Secret key has been reset.')
+  })
+})
+
+describe('mint module', () => {
+  test('it should ask about minting folder', async () => {
+    const output = await cli('mint')
+    expect(output).toContain('Which folder?')
+  })
+
+  test('it should warn about missing key', async () => {
+    filesystem.remove(TEZOS_CONFIG)
+    const output = await cli('mint ./data')
+    expect(output).toContain('Please set the secret key!')
+  })
+
+  test('it should mint a token', async () => {
+    await filesystem.write(TEZOS_CONFIG, wallet)
+    const output = await cli('mint ./__tests__/__mocks__/')
+    expect(output).toContain('Reading input...')
+    expect(output).toContain('Parsing input...')
+    expect(output).toContain('Minting...')
+    expect(output).toContain('Adding testimages_screenshot.jpg to IPFS.')
+    expect(output).toContain('Adding testimages_screenshot.jpg to IPFS.')
+    expect(output).toContain('Adding testimages_screenshot.jpg to IPFS.')
+    expect(output).toContain('Adding testimages_screenshot.jpg to IPFS.')
+    expect(output).toContain('Minting transaction confirmed!')
+  })
+
+  test('it should warn about missing entries', async () => {
+    const output = await cli('mint ./data')
+    expect(output).toContain('Reading input...')
+    expect(output).toContain('Parsing input...')
+    expect(output).toContain('No entries in CSV!')
+  })
+})
 
 describe('hicdex module', () => {
   test('gets creator for token 667691', async () => {
@@ -51,30 +144,3 @@ describe('hicdex module', () => {
     expect(output).toContain('Latest price of OBJKT #66769134 undefined')
   })
 })
-
-describe('setup module', () => {
-  test('setup secret key', async () => {
-    const output = await cli('setup')
-  })
-})
-
-
-
-//
-// test('outputs help', async () => {
-//   const output = await cli('--help')
-//   expect(output).toContain('0.0.1')
-// })
-//
-// test('generates file', async () => {
-//   const output = await cli('generate foo')
-//
-//   expect(output).toContain('Generated file at models/foo-model.ts')
-//   const foomodel = filesystem.read('models/foo-model.ts')
-//
-//   expect(foomodel).toContain(`module.exports = {`)
-//   expect(foomodel).toContain(`name: 'foo'`)
-//
-//   // cleanup artifact
-//   filesystem.remove('models')
-// })
---
+++ __tests__/helpers.ts
@@ -0,0 +1,35 @@
+import { filesystem } from 'gluegun'
+const src = filesystem.path(__dirname, '..')
+
+export const generateConfig = (operatorContract, marketplaceContract, minterContract) => {
+    console.log(`Generating config for ${operatorContract} and ${marketplaceContract}`)
+    filesystem.write(filesystem.path(src, 'src', 'hentools.config.js'),
+        `module.exports = {
+                  name: 'hentools',
+                  defaults: {
+                    rpcURL: 'http://localhost:20000',
+                
+                    // IPFS
+                    infuraURL: 'https://ipfs.somaticbits.xyz',
+                
+                    // contracts
+                    hicetnuncOperator: '${operatorContract}',
+                    teiaMarketplace: '${marketplaceContract}',
+                    hicetnuncMinter: '${minterContract}',
+                
+                    // OBJKTs
+                    defaultMetadata: {
+                      symbol: 'OBJKT',
+                      displayUri: '',
+                      thumbnailUri: 'ipfs://QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc',
+                      decimals: 0,
+                      isBooleanAmount: false,
+                      shouldPreferSymbol: false
+                    },
+                
+                    // tezos
+                    txConfirmations: 2
+                  }
+                }
+        `)
+}
